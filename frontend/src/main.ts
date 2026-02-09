da732ad81ebcca78fc3341771bfbd543799ff5b7
Ksaber Saber <ksaber@octiga.com>
2026-02-09 20:19:26 +0200

    prime ng

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,8 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { authInterceptor } from './app/core/interceptors/auth.interceptor';
@@ -15,6 +17,14 @@ bootstrapApplication(AppComponent, {
     provideHttpClient(
       withInterceptors([authInterceptor, errorInterceptor])
     ),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false,
+        }
+      }
+    }),
     MessageService,
   ],
 }).catch((err) => console.error(err));
